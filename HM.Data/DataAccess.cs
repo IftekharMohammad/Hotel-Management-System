aa870c5acd21d84dc1bf53b25b6e1218a52ef2db
Iftekhar Mohammad <im.toufiq@gmail.com>
2018-11-21 20:36:20 +0600

    Add files via upload

---
+++ HM.Data/DataAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace HM.Data
+{
+    public class DataAccess
+    {
+		private static SqlConnection sqcon;
+
+		public static SqlConnection Sqcon
+		{
+			get
+			{
+				if (sqcon == null)
+				{
+					
+					sqcon = new SqlConnection(@"Data Source=DESKTOP-V11AV1Q\SQLEXPRESS;Initial Catalog=HOTEL;Integrated Security=True");
+				}
+				else if (sqcon.State != ConnectionState.Open)
+				{
+					sqcon.Open();
+				}
+				return sqcon;
+			}
+		}
+
+		public static DataSet GetDataSet(string query)
+		{
+			SqlCommand sqcom = new SqlCommand(query, Sqcon);
+			SqlDataAdapter sda = new SqlDataAdapter(sqcom);
+			DataSet ds = new DataSet();
+			sda.Fill(ds);
+			return ds;
+		}
+
+		public static DataTable GetDataTable(string query)
+		{
+			var ds = GetDataSet(query);
+			if (ds.Tables.Count > 0)
+			{
+				return ds.Tables[0];
+			}
+			return null;
+		}
+
+		public static int ExecuteQuery(string query)
+		{
+			SqlCommand sqcom = new SqlCommand(query, Sqcon);
+			return sqcom.ExecuteNonQuery();
+		}
+	}
+}
